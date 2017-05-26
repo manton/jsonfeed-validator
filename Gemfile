e7550b017f816f3044de0dc6707806bbf98b8142
Manton Reece <manton@me.com>
2017-05-26 09:53:24 -0500

    Added basic feed downloading and parsing.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '2.3.4'
 
 gem 'sinatra'
 gem 'json-schema'
--- validator.rb
+++ validator.rb
@@ -1,5 +1,79 @@
 require 'sinatra'
+require 'net/http'
+require 'json-schema'
+
+JSON_SCHEMA = JSON.parse(IO.read(settings.root + "/config/schema.json"))
+
+class FeedError < Exception
+  attr_accessor :type
+  attr_accessor :message
+
+  def initialize(type, message)
+    @type = type
+    @message = message
+  end
+end
+
+def download_feed(jsonfeed_url, limit = 5)
+  if limit == 0
+    raise FeedError.new("error", "Too many redirects.")
+  end
+
+  s = ""
+
+  begin
+    uri = URI(jsonfeed_url)
+    req = Net::HTTP::Get.new(uri)
+    req["User-Agent"] = "JSONFeedValidator/1.0"
+
+    response = Net::HTTP.start(uri.hostname, uri.port, open_timeout: 30, read_timeout: 30, use_ssl: (uri.scheme == "https")) do |http|
+      http.request(req)
+    end
+
+    if response.is_a?(Net::HTTPSuccess)
+      s = response.body
+      if !response["content-type"].include?("application/json")
+        raise FeedError.new("error", "Content-Type was #{response['content-type']}. It should be application/json.")
+      end
+    elsif response.is_a?(Net::HTTPRedirection)
+      location = response["location"]
+      s = download_feed(location, limit - 1)
+    elsif response.is_a?(Net::HTTPNotFound)
+      raise FeedError.new("error", "404. No feed was found at this URL.")
+    elsif response.is_a?(Net::HTTPRequestTimeOut) || response.is_a?(Net::HTTPGatewayTimeOut)
+      raise FeedError.new("error", "Timeout downloading the feed.")
+    else
+      logger.info "Feed: Unknown error #{response.code} #{response.message}, #{response.class.name}"
+      raise FeedError.new("error", "Unknown error #{response.code} #{response.message}, #{response.class.name}.")
+    end
+  rescue FeedError => e
+    raise e
+  rescue Exception => e
+    logger.info "Feed: Unknown exception #{e.class.name}"
+    raise FeedError.new("error", "Unknown exception #{e.class.name}.")
+  end
+      
+  return s
+end
 
 get '/' do
-  return "Hello."
+  @url = params[:url].to_s
+  @errors = []
+
+  if @url.length > 0
+    begin
+      s = download_feed(@url)
+      response_json = JSON.parse(s)
+      results = JSON::Validator.fully_validate(JSON_SCHEMA, response_json)
+      for result in results
+        @errors << FeedError.new("error", result)
+      end
+    rescue JSON::ParserError => e
+      @errors << FeedError.new("error", "JSON could not be parsed. #{e.message}.")
+    rescue FeedError => e
+      @errors << e
+    end
+  end
+  
+  erb :index
 end
